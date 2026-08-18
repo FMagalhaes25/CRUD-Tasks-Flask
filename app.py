aff1bd00faeae5124752c667a6a5482349456b49
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-18 11:34:37 -0300

    feat: GET and POST methods

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
+
 
 # __name__ = "__main__"
 app = Flask(__name__)
 
-@app.route("/first")
-def hello_world():
-    return "Hello World"
+# CRUD
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def sobre():
-    return "Pagina sobre"
+#POST
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id= task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+
+# GET all
+@app.route("/tasks", methods=['GET'])
+def ver_tarefas():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+        
+    output = {
+        "tasks": task_list,
+        "total_tasks": 0
+    }
+    return jsonify(output)
+        
+    
 
 # Somente para ambiente de desenvolvimento
 if __name__ == "__main__":

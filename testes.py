8db64ede54ba8ca8ada6bf7489620bbf553d8957
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-18 12:54:21 -0300

    GET by id unit test

--- testes.py
+++ testes.py
@@ -23,5 +23,14 @@ def test_get_tasks():
     response_json = response.json()
     assert "tasks" in response_json
     assert "total_tasks" in response_json
+    
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
+
         
         
